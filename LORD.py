a80fe41ff7600fa8adf4408a3dc76a6db87af5cb
LORD <161044920+LORD-2@users.noreply.github.com>
2024-05-31 19:16:19 +0300

    Update LORD.py

--- LORD.py
+++ LORD.py
@@ -1,19 +1,19 @@
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import youtube_dl
 import os
 
 TOKEN = os.getenv('TELEGRAM_TOKEN')
 
-def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('أهلاً! استخدم الأمر /تحميل متبوعًا باسم الأغنية للبحث عنها وتنزيلها.')
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text('أهلاً! استخدم الأمر /تحميل متبوعًا باسم الأغنية للبحث عنها وتنزيلها.')
 
-def download(update: Update, context: CallbackContext) -> None:
+async def download(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     query = ' '.join(context.args)
     if not query:
-        update.message.reply_text('يرجى إدخال اسم الأغنية بعد الأمر /تحميل.')
+        await update.message.reply_text('يرجى إدخال اسم الأغنية بعد الأمر /تحميل.')
         return
-
+    
     ydl_opts = {
         'format': 'bestaudio/best',
         'outtmpl': '%(title)s.%(ext)s',
@@ -25,18 +25,16 @@ def download(update: Update, context: CallbackContext) -> None:
         info_dict = ydl.extract_info(f"ytsearch:{query}", download=True)
         file_name = ydl.prepare_filename(info_dict['entries'][0])
 
-    update.message.reply_audio(audio=open(file_name, 'rb'))
+    await update.message.reply_audio(audio=open(file_name, 'rb'))
     os.remove(file_name)
 
 def main() -> None:
-    updater = Updater(TOKEN, use_context=True)
-    dispatcher = updater.dispatcher
+    application = ApplicationBuilder().token(TOKEN).build()
 
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("تحميل", download))
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("تحميل", download))
 
-    updater.start_polling()
-    updater.idle()
+    application.run_polling()
 
 if __name__ == '__main__':
     main()
